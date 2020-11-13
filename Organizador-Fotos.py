e554acbee6dabad35fb14f5799901baa9ab4cfa6
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2020-11-13 18:09:24 -0300

    Update Organizador-Fotos.py

--- Organizador-Fotos.py
+++ Organizador-Fotos.py
@@ -3,7 +3,7 @@ from datetime import datetime
 # https://wp.stolaf.edu/it/installing-pil-pillow-cimage-on-windows-and-mac/
 # What is PIL/Pillow?   PIL (Python Imaging Library) adds many image processing features to Python. Pillow is a fork of PIL that adds some user-friendly features.
 
-# import PIL as pillow
+
 from PIL import Image
 
 
