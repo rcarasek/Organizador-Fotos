1c3d71604e77effe2b0add973284550207fd1faa
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2020-11-18 14:09:36 -0300

    Update Organizador-Fotos.py

--- Organizador-Fotos.py
+++ Organizador-Fotos.py
@@ -1,21 +1,24 @@
+# Installing
+# A step by step series of examples that tell you how to get a development env running 
+# Install Python 3.x with pip
+# Install Pillow, a Python Imaging Library
+# pip install Pillow
+# Install PyInstaller, to generate .exe file (for Windows)
+# pip install pyinstaller
+
 import os
 from datetime import datetime
 from PIL import Image
 
 def folder_path_from_photo_date(file):
     date = photo_shooting_date(file)
-    return date.strftime('%Y') + '/' + date.strftime('%Y-%m-%d')
-
+    return date
 
 def photo_shooting_date(file):
     photo = Image.open(file)
-    info = getexif()
-    if 36867 in info:
-        date = info[36867]
-        date = datetime.strftime(date, '%Y:%m:%d %h:%m:%s')
-    else:
-        date = datetime.fromtimestamp(os.path.getmtime(file))
-    return date
+    info = photo._getexif()
+    print(info)
+    return info
 
 print(folder_path_from_photo_date('IMG_1269.PNG'))
   
